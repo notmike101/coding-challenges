764bf75dae184f528f533d452a7c7384b50fafc8
Mike Orozco <me@mikeorozco.dev>
2023-10-02 21:00:35 -0500

    Add stats to problem 2627

--- leetcode/2627/index.ts
+++ leetcode/2627/index.ts
@@ -1,12 +1,20 @@
+/**
+ * LeetCode Debounce problem: https://leetcode.com/problems/debounce/description/
+ * Mike Orozco (notmike101)
+ * 
+ * Runtime: 57ms  - Beats 78.5%
+ * Memory: 42.5MB - Beats 79.33%
+ */
+
 type F = (...args: number[]) => void
 
 const debounce = (fn: F, t: number): F => {
   let debounced: ReturnType<typeof setTimeout>;
 
-  return function(...args) {
+  return (...args) => {
     clearTimeout(debounced);
 
-    debounced = setTimeout(function() {
+    debounced = setTimeout(() => {
       fn(...args)
     }, t);
   };
