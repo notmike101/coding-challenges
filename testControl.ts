0007ce0ebe471a4ca8fd385bd9076224fad09d39
Mike Orozco <me@mikeorozco.dev>
2023-12-08 19:29:14 -0600

    Update testControl to allow max output string size

--- testControl.ts
+++ testControl.ts
@@ -6,9 +6,9 @@ export interface ITest {
   testFunc?: (result: any, expected: any) => boolean;
 }
 
-export const captureRuntime = (func: Function, input: any): [number, number] => {
+const captureRuntime = (func: Function, input: any): [number, number] => {
   const startTime = performance.now();
-  let result;
+  let result: any;
 
   if (typeof input === "object") {
     result = func.apply(this, input);
@@ -21,7 +21,7 @@ export const captureRuntime = (func: Function, input: any): [number, number] => 
   return [result, runTime];
 };
 
-export const captureTestResults = (tests: ITest[]) => {
+export const captureTestResults = (tests: ITest[], maxOutputStringLength = Number.MAX_SAFE_INTEGER) => {
   const output: {[key: string]: any} = [];
 
   for (const { input, expected, func, identifier, testFunc } of tests) {
@@ -32,10 +32,10 @@ export const captureTestResults = (tests: ITest[]) => {
     
       output.push({
         IDENTIFIER: identifier ?? singleFunction.name,
-        INPUT: input.toString().length >= 15 ? `${input.toString().substring(0, 15)}...(${input.toString().length})` : input.toString(),
+        INPUT: input.toString().length > maxOutputStringLength ? `${input.toString().substring(0, maxOutputStringLength)}...(${input.toString().length - maxOutputStringLength})` : input.toString(),
         EXPECTED: expected.toString(),
-        RESULT: result.toString(),
-        PASS: testFunc?.(result, expected) ?? result.toString() === expected.toString(),
+        RESULT: result?.toString(),
+        PASS: testFunc?.(result, expected) ?? result?.toString() === expected.toString(),
         'RUNTIME (ms)': runTime,
       });
     }
