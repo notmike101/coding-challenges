9a72838e9cb793a5c72669fe36d9157247daa0f9
Mike Orozco <me@mikeorozco.dev>
2023-12-08 19:43:17 -0600

    Add leetcode 2620

---
+++ leetcode/2620.ts
@@ -0,0 +1,34 @@
+/**
+ * LeetCode Counter
+ * https://leetcode.com/problems/counter/description/
+ * Mike Orozco (notmike101)
+ * 
+ * Runtime: 
+ * Memory:  
+ */
+
+import { captureTestResults, type ITest } from '../testControl';
+
+const createCounter = (n: number): () => number => {
+  let counter = n;
+
+  return (): number => counter++;
+};
+
+const testFunc = (input: number, calls: number): number[] => {
+  const outputs: number[] = [];
+  const counter = createCounter(input);
+
+  for (let i = calls; i !== 0; --i) {
+    outputs.push(counter());
+  }
+
+  return outputs;
+};
+
+const tests: ITest[] = [
+  { input: [10, 3], expected: [10, 11, 12], func: testFunc },
+  { input: [-2, 5], expected: [-2, -1, 0, 1, 2], func: testFunc },
+]
+
+console.table(captureTestResults(tests));
