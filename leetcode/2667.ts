e47785f53a756adebceaace051f999b5ba04c666
Mike Orozco <me@mikeorozco.dev>
2024-01-06 22:34:00 -0600

    Add leetcode/2667

---
+++ leetcode/2667.ts
@@ -0,0 +1,10 @@
+/**
+ * Create Hello World Function
+ * https://leetcode.com/problems/create-hello-world-function/description/
+ * Mike Orozco (notmike101)
+ * 
+ * Runtime: 57ms - Beats 50.62%
+ * Memory:  42.78 MB - Beats 64.62%
+ */
+
+const createHelloWorld = () => (): string => 'Hello World';
