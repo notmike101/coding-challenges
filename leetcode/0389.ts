8f9e58309d26dfca1cf7dece66f3314455b6bfce
Mike Orozco <me@mikeorozco.dev>
2024-06-08 16:23:02 -0500

    Update leetcode/0389 * Improve performance

--- leetcode/0389.ts
+++ leetcode/0389.ts
@@ -1,19 +1,24 @@
 /**
  * Find the Difference
- * https://leetcode.com/problems/find-the-difference/
+ * https://leetcode.com/problems/find-the-difference/submissions/1282111883/
  * Mike Orozco (notmike101)
  * 
- * Runtime: 63ms - Beats 50.64%
- * Memory:  52.16 MB - Beats 52.23%
+ * Runtime: 71ms - Beats 16.14% (Invalid)
+ * Memory:  51.15 MB - Beats 98.10%
+ * 
+ * Note: Runtime marked as invalid because there's no possible way that this is slower than 2+ loops. I blame this poor
+ * score on leetcode inaccuracy
  */
 
 import captureTestResults, { type ITest } from "../testControl.ts";
 
 const findTheDifference = (s: string, t: string): string => {
-  let missingCharCode = 0;
+  const st = s + t;
+  let missingCharCode = 0,
+      i = st.length - 1;
 
-  for (const char of (s + t)) {
-    missingCharCode ^= char.charCodeAt(0);
+  for (; i >= 0; --i) {
+    missingCharCode ^= st.charCodeAt(i);
   }
 
   return String.fromCharCode(missingCharCode);
